331a0bbec8e3c1144ffd8c6717a8cd42b4ab4164
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-04-23 10:15:38 +0100

    Fixed bugs in configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,6 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: "development",
-    entry: "./src/index.js",
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
